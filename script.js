be1b419494bc5ddb3bac7f1fd555d8498ef2b2da
Benjamin Grelk <ben@grelk.tech>
2023-10-27 22:01:46 -0500

    Add assignment links

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ function loadManifest() {
     json = JSON.parse(request.responseText);
     return json;
 }
+
+/**
+ * Add assignment to a list
+ * @param {string} course The name of the course
+ * @param {string} name The name of the assignment
+ * @param {string} folder The due date of the assignment
+ * @param {Element} list The list to add the assignment to
+ */
+function addAssignmentToList(course, name, folder, list) {
+    let li = $("<li></li>");
+    let a = $("<a></a>");
+
+    a.attr("href", `./${course}/${folder}`);
+    a.text(name)
+
+    li.append(a);
+    list.append(li);
+}
