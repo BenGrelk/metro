4b33b2da3dfe5e10ad9367ddc9d0d7edd2144d59
Benjamin Grelk <ben@grelk.tech>
2023-11-05 16:03:18 -0600

    Remove comment

--- 1526/Assignment9/Assignment9/SavingsAccount.cs
+++ 1526/Assignment9/Assignment9/SavingsAccount.cs
@@ -2,12 +2,6 @@ namespace Assignment9;
 
 public class SavingsAccount
 {
-    /*
-     * Private class variable called savingsBalance
-       Private readonly class variable called annualInterestRate
-       Public property called SavingsBalance that gets and sets a private class variable
-       Public read only property called AnnualInterestRate gets a private variable
-     */
     private decimal _savingsBalance;
     private readonly decimal _annualInterestRate;
     
