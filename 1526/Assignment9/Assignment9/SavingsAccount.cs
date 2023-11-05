937d4c37cc44153bdcb5263a3f56f658a5ff84eb
Benjamin Grelk <ben@grelk.tech>
2023-11-05 16:02:25 -0600

    Explicitly define SavingsAccount fields

--- 1526/Assignment9/Assignment9/SavingsAccount.cs
+++ 1526/Assignment9/Assignment9/SavingsAccount.cs
@@ -2,12 +2,26 @@ namespace Assignment9;
 
 public class SavingsAccount
 {
-    public decimal SavingsBalance { get; set; }
-    public decimal AnnualInterestRate { get; }
-
+    /*
+     * Private class variable called savingsBalance
+       Private readonly class variable called annualInterestRate
+       Public property called SavingsBalance that gets and sets a private class variable
+       Public read only property called AnnualInterestRate gets a private variable
+     */
+    private decimal _savingsBalance;
+    private readonly decimal _annualInterestRate;
+    
+    public decimal SavingsBalance
+    {
+        get => _savingsBalance;
+        set => _savingsBalance = value;
+    }
+    
+    public decimal AnnualInterestRate => _annualInterestRate;
+    
     public SavingsAccount(decimal savingsBalance, decimal annualInterestRate)
     {
-        SavingsBalance = savingsBalance;
-        AnnualInterestRate = annualInterestRate;
+        _savingsBalance = savingsBalance;
+        _annualInterestRate = annualInterestRate;
     }
 }
